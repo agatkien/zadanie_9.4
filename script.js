f11af90ecb823aab6692c352d9fd1bfc3817673f
Agata Snoch <agatasnoch@Agatas-MacBook-Air.local>
2017-08-14 16:55:14 +0400

    pliczki

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var i, star, j;
+function drawTree(n){
+    
+ for (i=0; i<=n; i++)
+ {
+     star = '';
+   for (j=0; j<i; j++)
+     {
+   star += '*';
+       }
+ console.log(star);
+ }
+}
+drawTree(10);
